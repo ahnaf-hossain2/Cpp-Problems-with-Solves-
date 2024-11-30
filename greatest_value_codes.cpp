a8eb5c1f4ee6f3ad554400a86c51073a8db9f557
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2024-11-30 12:02:14 +0600

    find the greates value among 2 value

---
+++ greatest_value_codes.cpp
@@ -0,0 +1,26 @@
+
+
+
+//Take two int values from the user and print the greatest among them.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int value1,value2;
+    cout<<"Give value 1: ";
+    cin>>value1;
+    cout<<"Give value 2: ";
+    cin>>value2;
+
+    if(value1>value2)
+    {
+        cout<<"Value 1 is greatest";
+    }
+    else
+    {
+        cout<<"Value 2 is the greatest";
+    }
+
+}
