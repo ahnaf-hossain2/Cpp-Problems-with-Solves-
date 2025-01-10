162d4266778230d55662ae6487f85a179cbc85a9
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-10 10:55:37 +0600

    refactor student grade calculator to use function for grade determination

--- student_grade_calculator.cpp
+++ student_grade_calculator.cpp
@@ -21,41 +21,97 @@ Use of if-else or switch statement for grade determination
 Clear and descriptive output of the grade
 */
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     double num;
+
+//     cout<<" Enter the mark : ";
+//     cin>>num;
+//     if(0<num && num<100)
+//     {
+//         if(80<num && num<=100)
+//         {
+//             cout<<"Grade = A"<<endl;
+//         }
+//         else if(60<=num && num<=80)
+//         {
+//             cout<<"Grade = B"<<endl;
+//         }
+//         else if(50<=num && num<60)
+//         {
+//             cout<<"Grade = C"<<endl;
+//         }
+//         else if(45<=num && num<50)
+//         {
+//             cout<<"Grade = D"<<endl;
+//         }
+//         else if(25<=num && num<45)
+//         {
+//             cout<<"Grade = E"<<endl;
+//         }
+//         else
+//         {
+//             cout<<"Grade = F"<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// Write a program that will ask the user to enter his/her marks (out of 100). Define a function that will display grades according to the marks entered as below:
+// Marks Grade
+// 91-100 AA
+// 81-90 AB
+// 71-80 BB
+// 61-70 BC
+// 51-60 CD
+// 41-50 DD
+// <=40 Fail
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    double num;
-
-    cout<<" Enter the mark : ";
-    cin>>num;
-    if(0<num && num<100)
+void Marks(double num) {
+    if(num>0 && num<=100)
     {
-        if(80<num && num<=100)
+        if(91<=num && num<=100)
         {
-            cout<<"Grade = A"<<endl;
+            cout<<"Grade = AA"<<endl;
         }
-        else if(60<=num && num<=80)
+        else if(81<=num && num<=90)
         {
-            cout<<"Grade = B"<<endl;
+            cout<<"Grade = AB"<<endl;
         }
-        else if(50<=num && num<60)
+        else if(71<=num && num<=80)
         {
-            cout<<"Grade = C"<<endl;
+            cout<<"Grade = BB"<<endl;
         }
-        else if(45<=num && num<50)
+        else if(61<=num && num<=70)
         {
-            cout<<"Grade = D"<<endl;
+            cout<<"Grade = BC"<<endl;
         }
-        else if(25<=num && num<45)
+        else if(51<=num && num<=60)
         {
-            cout<<"Grade = E"<<endl;
+            cout<<"Grade = CD"<<endl;
+        }
+        else if(41<=num && num<=50)
+        {
+            cout<<"Grade = DD"<<endl;
         }
         else
         {
             cout<<"Grade = F"<<endl;
         }
     }
+    else cout << "Invalid marks." << endl;
+}
+
+int main () {
+    double num;
+    cout << "Enter your marks: ";
+    cin >> num;
+    Marks(num);
     return 0;
 }
