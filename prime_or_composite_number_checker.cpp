2c7e9ee1b945a30cfbc5d11ad22ca937ba4e9923
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-10 09:06:40 +0600

    added function for prime composite

--- prime_or_composite_number_checker.cpp
+++ prime_or_composite_number_checker.cpp
@@ -5,32 +5,70 @@
 //So it will check if it has any factor or not.
 // If n is divisible by any number between 2 to n/2 then it will print composite and get out of the loop because of the break statement.
 
+////// Normal way: 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	int n;
+// 	cout<<"Enter any number : ";
+// 	cin>>n;
+
+// 	if (n <= 1) {
+//         cout << n << " is neither prime nor composite";
+//         return 0;
+//     }
+    
+//     bool flag = true; // If true then it will be a prime number.
+// 	for (int i=2; i < (n/2); i++) {
+// 		if (n%i == 0) {
+// 			flag = false; // If it is divisible by any number then it will be a composite number.
+// 			break;
+// 		}
+// 	}
+
+//     if (flag == false) {
+//         cout << n << "Is a composite Number";
+//     }
+//     else {
+//         cout << n << " Is a Prime Number";
+//     }
+// }
+
+
+//////Using function: 
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int n;
-	cout<<"Enter any number : ";
-	cin>>n;
+int Prime_Composite(int number) {
+    for (int i = 2; i < ( number / 2) ; i++)
+        {
+            if ( number % i == 0)
+            {
+                return 0; // If it is divisible by any number then it will be a composite number.
+            }
+        }
+        return 1; // If it is not divisible by any number then it will be a prime number.
+}
 
-	if (n <= 1) {
-        cout << n << " is neither prime nor composite";
-        return 0;
+int main () {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number == 0 || number == 1) {
+        cout << "Neither prime or composite.";
     }
-    
-    bool flag = true; // If true then it will be a prime number.
-	for (int i=2; i < (n/2); i++) {
-		if (n%i == 0) {
-			flag = false; // If it is divisible by any number then it will be a composite number.
-			break;
-		}
-	}
 
-    if (flag == false) {
-        cout << n << "Is a composite Number";
+    int result = Prime_Composite(number);
+    if (result == 1)
+    {
+        cout << "Prime Number";
     }
-    else {
-        cout << n << " Is a Prime Number";
-    }
+
+    else if (result == 0)
+        cout << "Composite Number";
+    else
+        cout << "Nor composite nor prime.";
 }
