77d3075df3abc1111b99e166cb721723550d026f
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-21 02:01:05 +0600

    add program to reverse an integer number containing only odd digits

--- reverse_number_odd.cpp
+++ reverse_number_odd.cpp
@@ -1,5 +1,5 @@
 /*
-Write a program in C++ to Reverse an Integer Number that will 
+Write a program in C++ to Reverse an Integer Number that will
 only contain the odd digits.
 Sample output:
 Enter an Integer Number: 123456
@@ -15,10 +15,28 @@ int main()
     cin>>number;
 
     while (number != 0 ) {
-        right_digit = number % 10; //1234%10=4 , 123%10=3 ..... 
+        right_digit = number % 10; //1234%10=4 , 123%10=3 .....
         if (right_digit % 2 != 0){
             cout << right_digit <<" ";
         }
         number = number / 10; //1234/10=123, 123/10=12 .....
     }
 }
+/*
+Alternative way:
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n,reverseNum=0,lastDigit;
+	cout <<"Enter a number: ";
+	cin >> n;
+	while(n!=0) {
+		lastDigit = n%10; //It gives the last digit
+		n = n/10;
+		reverseNum = reverseNum * 10 + lastDigit;
+	}
+	cout << reverseNum;
+}
+*/
