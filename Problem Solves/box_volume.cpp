25098f05fc63e0564cbd21e687406b8ea8910319
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-25 01:06:23 +0600

    add box volume calculator using class with setter and getter methods

---
+++ Problem Solves/box_volume.cpp
@@ -0,0 +1,60 @@
+// Problem: Write a class Box that has three private data members length, breadth and height. The class should have the following public member functions also use setter getter methods:
+#include <iostream>
+using namespace std;
+
+class Box {
+    private:
+        int length;
+        int breadth;
+        int height;
+    public:
+
+    float getLength() {
+        return length;
+    }
+    float getBreadth() {
+        return breadth;
+    }
+    float getHeight() {
+        return height;
+    }
+    void setLength(int l) {
+        if(l>=0) {
+            length = l;
+        } else {
+            length = 0;
+        }
+    }
+    void setBreadth(int b) {
+        if(b>=0) {
+            breadth = b;
+        } else {
+            breadth = 0;
+        }
+    }
+    void setHeight(int h) {
+        if(h>=0) {
+            height = h;
+        } else {
+            height = 0;
+        }
+    }
+    float returnVolume() {
+        return length*breadth*height;
+    }
+};
+
+int main() {
+    Box b;
+    cout << "Enter length, breadth and height of box: ";
+    int l, b, h;
+    cin >> l >> b >> h;
+    b.setLength(l);
+    b.setBreadth(b);
+    b.setHeight(h);
+    cout << b.getLength() << endl;
+    cout << b.getBreadth() << endl;
+    cout << b.getHeight() << endl;
+    cout << b.returnVolume() << endl;
+    return 0;
+}
