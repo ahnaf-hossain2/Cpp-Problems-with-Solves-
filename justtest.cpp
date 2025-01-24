ed0ae0b06dcec0fcd7fbbef8a78a95014ef23796
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-25 01:13:04 +0600

    just testing

--- justtest.cpp
+++ justtest.cpp
@@ -1,33 +1,37 @@
-#include<iostream>
+// A program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'. You must use private properties and public methods for this class. The output should be as follows:
+// Name        Year of joining        Address
+// Robert        1994        64C- WallsStreat
+// Sam        2000        68D- WallsStreat
+// John        1999        26B- WallsStreat
+
+#include <iostream>
 using namespace std;
 
-// Here I made a function with 2 parameters a and b
-void sum (int a, int b)
-{
-    cout << "Sum: " << a + b << endl;
-}
-
-// Here I made the same function with 3 parameters a, b, and c
-// this is function overloading.
-// if we made this function with 2 parameters then there will be an error.
-void sum (int a, int b, int c)
-{
-    cout << "Sum: " << a + b + c << endl;
-}
-
-// but if I change the data type of the same function then it will work.
-void sum (double a, double b)
-{
-    cout << "Sum: " << a + b << endl;
-}
+class Employee {
+    private:
+        string name;
+        int year;
+        string address;
+    public:
+        void setInfo(string n, int y, string a) {
+            name = n;
+            year = y;
+            address = a;
+        }
+        void getInfo() {
+            cout << name << "\t" << year << "\t" << address << endl;
+        }
+};
 
 int main()
 {
-    sum(5, 7);
-    sum(5, 7, 9);
-    sum(3.48, 5.00);
-    // but for float you need to code it like this:
-    // sum(3.48f, 5.00f);
-    // either the code will not work
-
+    Employee e1, e2, e3;
+    e1.setInfo("Robert", 1994, "64C- WallsStreat");
+    e2.setInfo("Sam", 2000, "68D- WallsStreat");
+    e3.setInfo("John", 1999, "26B- WallsStreat");
+    cout << "Name\tYear of joining\tAddress" << endl;
+    e1.getInfo();
+    e2.getInfo();
+    e3.getInfo();
+    return 0;
 }
