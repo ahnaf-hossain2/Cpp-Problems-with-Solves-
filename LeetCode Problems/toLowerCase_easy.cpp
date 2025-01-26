d4da74c8d2f8e1180f1c20082e3b7d8b65d5bcb1
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-01-26 14:46:06 +0600

    remove commented-out code and improve clarity in toLowerCase solution

--- LeetCode Problems/toLowerCase_easy.cpp
+++ LeetCode Problems/toLowerCase_easy.cpp
@@ -7,3 +7,17 @@ public:
         return s;
     }
 };
+
+// #include <iostream>
+// #include <string>
+// #include <cctype>
+// using namespace std;
+
+// int main() {
+// 	string text = "Hello";
+// 	for (int i=0; i<text.length(); ++i) {
+// 		text[i] = tolower(text[i]); // here tolower(text[i]) converts the full text to lowercase
+// 		// same way we can convert lowercase to upercase using toupper(text[i])
+// 	}
+// 	cout << text;
+// }
